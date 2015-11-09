cdc36848663b1a3c8a752904002e54fe1312c27c
Tony Blum <tonyblum@me.com>
2015-11-08 22:04:30 -0800

    second commit

---
+++ java.js
@@ -0,0 +1,144 @@
+setInterval(function() {
+  console.log(i);
+  i++;
+}, 1000);
+
+setInterval(function() {
+  console.log(".")
+}, 1000)
+
+
+
+var interval = setInterval(function() {
+  console.log(".")
+}, 1000);
+
+setTimeout(function(){
+  clearInterval(interval);
+}, 11000);
+
+var countDown = setInterval(function(){
+  if(count > 0) {
+    console.log(count + "...");
+    count -= 1;
+  } else {
+    console.log("Blast Off!!!")
+  }
+}, 1000);
+
+
+
+})
+setInterval(function() {
+  console.log(".");
+
+setTimeout(function(10) {
+
+})
+
+  function(setTimeout) {
+    set
+  }
+}, 1000)
+
+num = 55
+
+
+
+
+
+function age(num) {
+  if num > 50;
+  console.log("Old")
+} else {
+  console.log("Young")
+};
+  var total = x + y;
+  return total;
+};
+
+
+var age = function(num) {
+  if (num > 50) {
+  console.log("old")
+} else {
+  console.log("young")
+};
+
+
+var age = function(num) {
+  if (num > 50) {
+    console.log("Old")
+  } else {
+    console.log("Young")
+  };
+};
+
+
+var a = function([], func()) {
+  a.forEach()
+  for each |n| in [] do func1;
+
+}
+
+
+var a = ["a", "b", "c"];
+a.forEach(function(n) {
+    console.log(n);
+});
+
+
+  var later = sayLater(function(string) {
+    console.log(string);
+  }, 5000);
+
+sayLater(function(string) {
+  setTimeout(function() {
+
+  }, 5000);
+  console.log(string);
+}, 5000);
+
+
+setTimeout(function(){
+  clearInterval(interval);
+}, 11000);
+
+  var myVar = function(string) {
+    myVar = setTimeout(function(string) {
+      alert("Hello") }, 5000);
+
+
+
+  var person = function(age) {
+    if(age > 50) {
+      return "Old";
+    } else {
+      return("Young");
+    };
+  };
+
+
+  myArray = [1,2,3,4,5]
+
+  var forEach = function(array, func) {
+    for(var i = 0; i < array.length; i++) {
+      array[i] = func(array[i]);
+    }
+    return array;
+  }
+
+for
+
+
+
+//////\\\\\\\\||||||
+
+
+  var sayLater = function(string) {
+    setTimeout(function() {
+      console.log(string);
+    }, 5000)
+  }
+
+  sayLater("Hello Friday!")
